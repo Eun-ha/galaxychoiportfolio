9b464fcde3ed00a590f09d4609bb73dc6b5c9bf6
eunhasu <eunhasu@eunhasuui-MacBookPro.local>
2025-01-18 12:36:37 +0900

    Refactor: resume-content

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,31 @@
+import {
+  Descriptions,
+  Education,
+  Experience,
+  getDescriptions,
+  getEducations,
+  getExperiences,
+  getSkills,
+  Skill,
+} from "@/data/resume";
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+export function getData(type: string) {
+  switch (type) {
+    case "descriptions":
+      return getDescriptions();
+    case "educations":
+      return getEducations();
+    case "experiences":
+      return getExperiences();
+    case "skills":
+      return getSkills();
+    default:
+      return [];
+  }
 }
