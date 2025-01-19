e3f44d26c7a0a26304faeca1cbbc8ae5ea28a682
eunhasu <eunhasu@eunhasuui-MacBookPro.local>
2025-01-19 18:36:51 +0900

    suspense test2

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,7 +1,7 @@
 export default function Loading() {
   return (
     <div className="space-y-4 text-center mt-[25%]">
-      <span className="text-xl font-medium text-gray-400/80">
+      <span className="text-xl font-medium text-red-300">
         App loading 로딩 중...
       </span>
     </div>
