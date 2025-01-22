e8eda027af9d23eed9ee472097179759db9841c4
eunhasu <eunhasu@eunhasuui-MacBookPro.local>
2025-01-22 12:55:49 +0900

    add env

--- app/resume/[slug]/page.tsx
+++ app/resume/[slug]/page.tsx
@@ -7,7 +7,8 @@ type tParams = Promise<{ slug: string }>;
 
 export default async function Page(props: { params: tParams }) {
   const { slug } = await props.params;
-  const data = await fetchData(`http://localhost:3001/api/resume/${slug}`);
+  const ApiUrl = process.env.API_URL;
+  const data = await fetchData(`${ApiUrl}/api/resume/${slug}`);
 
   return (
     <main className="block w-full lg:w-[calc(100%-16px)] lg:ml-[16px]">
