2cc8f049566cf5158b3a7daf2fe7d423205fbe70
eunhasu <eunhasu@eunhasuui-MacBookPro.local>
2025-01-29 15:17:19 +0900

    Add font file https://notion.so/TASK-45

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,16 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "@/styles/globals.css";
 import { Header } from "@/components/header";
 import { DarkTheme } from "@/components/dark-theme";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { notoSansKR } from "@/components/fonts";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -26,11 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-bg-default`}
-      >
+      <body className={`${notoSansKR.className} antialiased`}>
         <DarkTheme />
-        <div className="wrapper overflow-hidden">
+        <div className="wrapper overflow-hidden main">
           <Header />
           {children}
         </div>
