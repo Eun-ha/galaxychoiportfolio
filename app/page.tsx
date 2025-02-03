629128d0848e9d32f32aeb6f3e9dad09d6f7b5f4
eunhasu <eunhasu@eunhasuui-MacBookPro.local>
2025-02-03 11:13:13 +0900

    백엔드 테스트

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { getData } from "@/backend/account-action";
 import Decoration from "@/components/main/decoration";
 import HomeAbout from "@/components/main/home-about";
 import HomeContact from "@/components/main/home-contact";
@@ -6,7 +7,7 @@ import { HomeParticles } from "@/components/main/home-particles";
 import HomeSkills from "@/components/main/home-skills";
 import ScrollTrackerNav from "@/components/main/scroll-tracker-nav";
 
-export default function Home() {
+export default async function Home() {
   return (
     <div className="relative z-30 bg-darkOnly-bg h-full px-4 py-4 lg:px-[100px] lg:py-[80px]">
       <div id="00" className="py-10 lg:py-[150px">
