f76d415fbf98d0cb4dd2396a97f7e289aa5e051b
eunhasu <eunhasu@eunhasuui-MacBookPro.local>
2025-03-12 17:56:36 +0900

    aside button animate ping 효과 수정

--- components/resume/aside-button.tsx
+++ components/resume/aside-button.tsx
@@ -23,7 +23,7 @@ export const AsideButton = ({
     <Link
       href={href}
       className={clsx(
-        "relative py-4 lg:pl-4 lg:block lg:rounded-lg lg:hover:bg-bg-hover capitalize",
+        "inline-block py-4 lg:pl-4 lg:block lg:rounded-lg lg:hover:bg-bg-hover capitalize",
         {
           "lg:bg-bg-active text-point-red": isActive,
         }
@@ -32,15 +32,7 @@ export const AsideButton = ({
     >
       <span
         className={clsx({
-          "absolute inline-block h-[60%] w-full animate-ping rounded-md  bg-amber-950 opacity-75 lg:hidden":
-            isActive,
-        })}
-      ></span>
-
-      <span
-        className={clsx({
-          "relative p-2 rounded-md bg-amber-950 lg:p-0 lg:bg-transparent":
-            isActive,
+          "p-2 rounded-md bg-amber-950 lg:p-0 lg:bg-transparent": isActive,
         })}
       >
         {category.button}
--- components/resume/titles-descriptions.tsx
+++ components/resume/titles-descriptions.tsx
@@ -1,4 +1,5 @@
 import { TitleAndDescription } from "@/data/resume";
+import { Ping } from "../ui/ping";
 
 type Props = {
   slug: string;
@@ -8,7 +9,10 @@ export const TitlesDescriptions = (props: Props) => {
   const data = TitleAndDescription.find((e) => e.slug === props.slug);
   return (
     <section className="my-5 lg:mt-0">
-      <h2 className="text-lg font-medium">{data?.title}</h2>
+      <h2 className="text-lg font-medium flex items-center">
+        <Ping />
+        <span>{data?.title}</span>
+      </h2>
       <p className="text-text-emphasis">{data?.description}</p>
     </section>
   );
