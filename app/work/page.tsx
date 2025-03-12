1e2498ff093a071512b7d3ac1f276c8da78fcb25
eunhasu <eunhasu@eunhasuui-MacBookPro.local>
2025-03-12 15:27:49 +0900

    isr test

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -8,6 +8,8 @@ export const metadata: Metadata = {
     "대표 작업물에 대한 설명과 함께 링크, 소스 다운로드 경로, 깃 경로를 제공합니다.",
 };
 
+export const revalidate = 3;
+
 export default async function Work() {
   const ApiUrl = process.env.PRODUCTION_URL;
   const data = await fetchData(`${ApiUrl}/api/work`);
